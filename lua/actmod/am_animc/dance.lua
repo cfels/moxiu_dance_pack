f101522fb7939ba7aef1b33ff0fc8a3c509015a0
𝙈𝙤𝙭𝙞𝙪 💤 <moxiix@proton.me>
2026-09-11 22:39:40 +0200

    add one more dance both of them still need SYNC!

--- lua/actmod/am_animc/dance.lua
+++ lua/actmod/am_animc/dance.lua
@@ -14,6 +14,41 @@ return {
     -- "Amod_Fortnite_Lineage"
     -- "Amod_Fortnite_Nimble"
     -- "Amod_Fortnite_Noodles"
+	["ytpmv"] = {
+		Config = {
+			Anim = "amod_fortnite_lineage",
+			Name = "YTPMV - Kwik Flip",
+			class = 1,
+			isNew = true
+		},
+		Camera = {
+			Follow = false,
+			Transition = 1.5
+		},
+		Custom = {
+			Anim = {
+				Time1 = 10.462,
+				Time2 = 6.99,
+				Cycle = 0.33333334326744
+			},
+			Sound = {
+				Time1 = 200.12,
+				Time2 = 200.12
+			}									
+		},	
+		Coop = {  
+			Sync = true,
+			MaxDist = 400
+		},
+		Sounds = {
+			Start = {
+				Sound = "actmod/AB/ytpmv.mp3",
+				Delay = 0
+			}
+		}
+	},
+	
+
     ["girldidwhat"] = {
         Config = {
             Anim = "amod_fortnite_noodles",
